8071185351a30eb0c33c0b0fef58f60b3be3de78
Yash Chandra Verma <ycverma005@gmail.com>
2018-02-26 17:53:06 +0530

    Update and rename Full_Adder.v to Full_Adder_Circuit.v

--- Full_Adder_Circuit.v
+++ Full_Adder_Circuit.v
@@ -1,21 +1,10 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    16:10:33 02/26/2018 
-// Design Name: 
-// Module Name:    Full_Adder 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
+// By: Yash Chandra Verma
 //
-// Dependencies: 
+// Module Name:    Full Adder Circuit 
 //
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
+// Description: A Normal Full Adder Circuit using XOR and AND Gates
 //
 //////////////////////////////////////////////////////////////////////////////////
 module Full_Adder(
